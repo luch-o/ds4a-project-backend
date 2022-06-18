10a0bee646cbf3691c101d566d866773d9f2757f
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2022-06-18 12:09:12 -0500

    auto parquetization of files stored in datalake bucket

--- src/parquetize.py
+++ src/parquetize.py
@@ -1,12 +1,50 @@
+import io
 import json
 import boto3
 import pandas as pd
 
 
-def handler(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+def sizeof_fmt(num, suffix="B"):
+    """
+    Human readable file size. Taken from https://stackoverflow.com/a/1094933
+    """
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+
+s3 = boto3.client("s3")
+
+
+def handler(event, context):
+    for record in event["Records"]:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+        size = record["s3"]["object"]["size"]
+
+        print(f"Loading s3://{bucket}/{key} of size {sizeof_fmt(size)} bytes")
+        csv_bytes = s3.get_object(Bucket=bucket, Key=key).get("Body")
+        df = pd.read_csv(csv_bytes)
+
+        pq_key = key.replace("cleaned", "parquet").replace("csv", "parquet")
+        with io.BytesIO() as pq_buffer:
+            df.to_parquet(pq_buffer, index=False)
+            s3.put_object(Bucket=bucket, Key=pq_key, Body=pq_buffer.getvalue())
+
+        pq_size = s3.head_object(Bucket=bucket, Key=pq_key).get("ContentLength")
+        print(
+            f"Stored s3://{bucket}/{pq_key} of size {sizeof_fmt(pq_size)} bytes.",
+            f"Size reduction of {(size-pq_size)/size:.2%}",
+        )
+
+        s3.delete_object(Bucket=bucket, Key=key)
+
+    return {"statusCode": 200, "body": {}}
+
+
+if __name__ == "__main__":
+    with open("test-event.json") as jsonfile:
+        event = json.load(jsonfile)
+    handler(event, None)
